6f39d5868b19d57fad5da922a02c885205788b4e
Vladimir Novichikhin <darkonica@gmail.com>
2024-01-18 13:24:43 +0300

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const Application: React.FC = () => {
+  return (
+    <div id='app'>
+      Here we go
+    </div>
+  );
+};
+
+export default Application;
