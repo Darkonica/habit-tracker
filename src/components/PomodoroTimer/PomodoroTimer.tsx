16cf2c9dd0c91216e0f5313d50805394163af8c0
Vladimir Novichikhin <darkonica@gmail.com>
2024-01-23 17:34:21 +0300

    Pomodoro done

--- src/components/PomodoroTimer/PomodoroTimer.tsx
+++ src/components/PomodoroTimer/PomodoroTimer.tsx
@@ -3,13 +3,29 @@ import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-pro
 import 'react-circular-progressbar/dist/styles.css';
 import styles from './styles.scss';
 
-const DEFAULT_TIMER = 1500;
+// enum DefaultTimer {
+//   POMODORO = 1500,
+//   LONG_BREAK = 900,
+//   SHORT_BREAK = 300,
+// }
+// Just for testing
+enum DefaultTimer {
+  POMODORO = 5,
+  LONG_BREAK = 4,
+  SHORT_BREAK = 3,
+}
+enum CurrentActivity {
+  POMODORO = "pomodoro",
+  LONG_BREAK = "long break",
+  SHORT_BREAK = "short break",
+} 
 
 export const PomodoroTimer = () => {
   const [timer, setTimer] = useState(5); // in seconds
   const [pomodoroCount, setPomodoroCount] = useState(0);
+  const [currentActivity, setCurrentActivity] = useState(CurrentActivity.POMODORO);
   const [isPause, setIsPause] = useState(false);
-
+  
   const timerId = useRef<ReturnType<typeof setInterval>>(null);
 
   useEffect(() => {
@@ -18,9 +34,27 @@ export const PomodoroTimer = () => {
     }
 
     if (timer < 1) {
-      setPomodoroCount(pomodoroCount + 1);
+      // Chain of breaks and pomodoro
+      if (currentActivity !== CurrentActivity.POMODORO) {
+        setCurrentActivity(CurrentActivity.POMODORO);
+        setTimer(DefaultTimer.POMODORO);
+      }
+
+      if (currentActivity === CurrentActivity.POMODORO) {
+        const updatedPomodoroCount = pomodoroCount + 1;
+
+        if (updatedPomodoroCount % 4 === 0) {
+          setCurrentActivity(CurrentActivity.LONG_BREAK);
+          setTimer(DefaultTimer.LONG_BREAK);
+        } else {
+          setCurrentActivity(CurrentActivity.SHORT_BREAK);
+          setTimer(DefaultTimer.SHORT_BREAK);
+        }
+
+        setPomodoroCount(updatedPomodoroCount);
+      }
+
       clearInterval(timerId.current);
-      setTimer(DEFAULT_TIMER);
       setIsPause(true);
     }
 
@@ -37,11 +71,48 @@ export const PomodoroTimer = () => {
     setIsPause(!isPause);
   }
 
+  const handleCurrentActivityChange = (activity: CurrentActivity) => {
+    if (activity === currentActivity) return;
+
+    setCurrentActivity(activity);
+    clearInterval(timerId.current);
+
+    if (activity === CurrentActivity.POMODORO) {
+      setTimer(DefaultTimer.POMODORO);
+    }
+    if (activity === CurrentActivity.SHORT_BREAK) {
+      setTimer(DefaultTimer.SHORT_BREAK);
+    }
+    if (activity === CurrentActivity.LONG_BREAK) {
+      setTimer(DefaultTimer.LONG_BREAK);
+    }
+    setIsPause(true);
+  }
+
   const timerView = `${Math.trunc(timer / 60)}:${(timer % 60).toLocaleString(undefined, {minimumIntegerDigits: 2})}`
 
   return (
     <>
-      <h3 className={styles.title}>Pomodoro</h3>
+      <div className={styles.activityCont}>
+        <button
+          className={currentActivity === CurrentActivity.POMODORO && styles.active}
+          onClick={() => handleCurrentActivityChange(CurrentActivity.POMODORO)}
+        >
+          {CurrentActivity.POMODORO}
+        </button>
+        <button
+          className={currentActivity === CurrentActivity.SHORT_BREAK && styles.active}
+          onClick={() => handleCurrentActivityChange(CurrentActivity.SHORT_BREAK)}
+        >
+          {CurrentActivity.SHORT_BREAK}
+        </button>
+        <button
+          className={currentActivity === CurrentActivity.LONG_BREAK && styles.active}
+          onClick={() => handleCurrentActivityChange(CurrentActivity.LONG_BREAK)}
+        >
+          {CurrentActivity.LONG_BREAK}
+        </button>
+      </div>
       
       <div className={styles.progressbarCont}>
         <button className={styles.statusHiddenButton} onClick={handleStatusChange}>
@@ -59,9 +130,6 @@ export const PomodoroTimer = () => {
       </div>
 
       <div className={styles.pomodoroCount}>Today: {pomodoroCount}</div>
-
-      {/* Pomodoro, short break, long break */}
-      <div>Pomodoro / short break / long break</div>
     </>
   )
 }
